f41e4d7b8bcb4a30a90fb9525bbfb687632c4202
daksh2141 <dakshg277@gmail.com>
2026-01-20 16:08:46 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,7 @@ const app = express();
 
 
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:3000'],
+    origin:['http://localhost:5173','http://localhost:3000' , "https://thumblify-neon.vercel.app"],
     credentials:true
 }))
 
